b7a0caf9262bbe73d94daa1d0760774dd6cd577c
Gamaliel mendez <diana22288@gmail.com>
2020-08-30 13:01:46 -0500

    se agrega const al modulo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Mysql = require('./src/Mysql')
 const SyncDb = require('./src/SyncDb')
+const ConstansSync = require('./src/Consts')
 
 module.exports={
     Mysql,
-    SyncDb
+    SyncDb,
+    ConstansSync
 }
